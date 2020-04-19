b9285ade92a2777695147ae491f0f38fd295b23a
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2020-04-19 17:00:53 -0300

    Initial commit - handshake

---
+++ Handshake/Solution.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the handshake function below.
+     */
+    static int handshake(int n) {
+        /*
+         * Write your code here.
+         */
+         return (n * (n-1)) / 2;
+
+    }
+}
